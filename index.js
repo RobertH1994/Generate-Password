0dc29919ce01e9a6221358c407eeb89d82a352ff
Robert Hines IV <roberthines1994@yahoo.com>
2020-12-10 17:41:40 -0500

    added character var

---
+++ index.js
@@ -0,0 +1,24 @@
+// first i create var for lowercase, uppercase, numeric, and/or special characters
+// need function to prompt user for criteria
+// function gernerate password function
+//  need funtion makeid(arr)
+// added var
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0',]
+var specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')']
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
